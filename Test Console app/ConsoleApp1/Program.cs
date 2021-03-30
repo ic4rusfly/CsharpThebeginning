a7681b14725047d55ba5fc38c225128dfc019bbb
Garrosh HellScream <32456787+ic4rusfly@users.noreply.github.com>
2021-03-30 19:15:36 +0100

    innit

--- Test Console app/ConsoleApp1/Program.cs
+++ Test Console app/ConsoleApp1/Program.cs
@@ -4,9 +4,9 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to my console app xD!");
         }
     }
 }
